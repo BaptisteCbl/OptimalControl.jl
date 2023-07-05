609bbc6fad79e9382606f213f9db0e20f56f8d8d
Baptiste Combelles <baptiste.combelles@proton.me>
2023-07-05 15:43:14 +0200

    aggregation only

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,14 @@
 using Documenter
 using OptimalControl
 
-makedocs(
-    sitename = "OptimalControl.jl",
-    format = Documenter.HTML(prettyurls = false),
-    pages = [
-        "Introduction" => "index.md",
-        "API" => "api.md"
-    ]
-)
+# makedocs(
+#     sitename = "OptimalControl.jl",
+#     format = Documenter.HTML(prettyurls = false),
+#     pages = [
+#         "Introduction" => "index.md",
+#         "API" => "api.md"
+#     ]
+# )
 
 # deploydocs(
 #     repo = "github.com/control-toolbox/OptimalControl.jl.git",
