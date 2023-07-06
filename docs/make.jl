cb8a23bd54da13f0ef6d6405ee10274e1109f10a
Baptiste Combelles <baptiste.combelles@proton.me>
2023-07-06 14:49:20 +0200

    add gh page

--- docs/make.jl
+++ docs/make.jl
@@ -17,6 +17,6 @@ makedocs(
 
 deploydocs(
     #repo = "github.com/control-toolbox/OptimalControl.jl.git",
-    repo = "github.com/BaptisteCbl/OptimalControl.jl"#,
-    # devbranch = "main"
+    repo = "github.com/BaptisteCbl/OptimalControl.jl",
+    devbranch = "main"
 )
